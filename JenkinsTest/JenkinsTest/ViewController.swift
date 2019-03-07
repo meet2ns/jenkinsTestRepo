feff8e8c95f83b33e8006a7d4b8dd8275b0cecf3
Nauman Saleem <nauman@onebyte.biz>
2019-03-07 17:33:10 +0500

    - Project Setup

---
+++ JenkinsTest/JenkinsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsTest
+//
+//  Created by Nauman Saleem on 07/03/2019.
+//  Copyright © 2019 Nauman Saleem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
